c460df25123c6ac413ae4ed3c3fff71284b18bc7
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-02-28 15:43:14 +0300

    Bonus

---
+++ src/test/java/ru/netology/Servts/CashbackTest.java
@@ -0,0 +1,53 @@
+package ru.netology.Servts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackTest {
+    @Test
+    void serviceBonus() {
+        Cashback service = new Cashback();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+    @Test
+    void upToBuyAt564() {
+        Cashback service = new Cashback();
+        int amount = 436;
+        int actual = service.remain(amount);
+        int expected = 564;
+        assertEquals(actual, expected);
+    }
+    @Test
+    void upToBuyAt200() {
+        Cashback service = new Cashback();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(actual, expected);
+    }
+    @Test
+    void upToBuyAt900() {
+        Cashback service = new Cashback();
+        int amount = 100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+    @Test
+    void toBuy1200() {
+        Cashback service = new Cashback();
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 800;
+        assertEquals(actual, expected);
+    }
+
+}
+
+
+
+
