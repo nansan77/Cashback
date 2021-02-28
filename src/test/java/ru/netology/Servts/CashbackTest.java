419bbbd417ad75d6fd0491aa3a4f9413abc7ce84
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-02-28 22:47:08 +0300

    junit4

--- src/test/java/ru/netology/Servts/CashbackTest.java
+++ src/test/java/ru/netology/Servts/CashbackTest.java
@@ -9,8 +9,8 @@ import static org.junit.Assert.*;
     public void serviceBonus() {
         Cashback service = new Cashback();
         int amount = 1000;
-        int actual = service.remain(amount);
-        int expected = 0;
+        int expected = service.remain(amount);
+        int actual= 0;
         assertEquals(actual,expected);
 
     }
@@ -18,32 +18,32 @@ import static org.junit.Assert.*;
     public void upToBuyAt564() {
          Cashback service = new Cashback();
          int amount = 436;
-         int actual = service.remain(amount);
-         int expected = 564;
+         int expected = service.remain(amount);
+         int actual = 564;
          assertEquals(actual,expected);
      }
      @Test
      public void upToBuyAt200() {
          Cashback service = new Cashback();
          int amount = 800;
-         int actual = service.remain(amount);
-         int expected = 200;
+         int expected = service.remain(amount);
+         int actual = 200;
          assertEquals(actual,expected);
      }
      @Test
      public void upToBuyAt900() {
          Cashback service = new Cashback();
          int amount = 100;
-         int actual = service.remain(amount);
-         int expected = 900;
+         int expected = service.remain(amount);
+         int actual = 900;
          assertEquals(actual,expected);
      }
      @Test
      public void toBuy1200() {
          Cashback service = new Cashback();
          int amount = 1200;
-         int actual = service.remain(amount);
-         int expected = 800;
+         int expected = service.remain(amount);
+         int actual = 800;
          assertEquals(actual,expected);
      }
 }
