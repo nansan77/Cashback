41418e9e2a92686ed1528f9b8f027b8d395afcc1
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-03-09 22:20:44 +0300

    bonus

--- src/test/java/ru/netology/Servts/CashbackTest.java
+++ src/test/java/ru/netology/Servts/CashbackTest.java
@@ -11,39 +11,43 @@ class CashbackTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     void upToBuyAt564() {
         Cashback service = new Cashback();
         int amount = 436;
         int actual = service.remain(amount);
         int expected = 564;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     void upToBuyAt200() {
         Cashback service = new Cashback();
         int amount = 800;
         int actual = service.remain(amount);
         int expected = 200;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     void upToBuyAt900() {
         Cashback service = new Cashback();
         int amount = 100;
         int actual = service.remain(amount);
         int expected = 900;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     void toBuy1200() {
         Cashback service = new Cashback();
         int amount = 1200;
         int actual = service.remain(amount);
         int expected = 800;
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
 }
