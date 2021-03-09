28ae5cd2881afdfae8f3eef7230db0b97bf17bb7
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-03-09 21:59:12 +0300

    bonus

--- src/test/java/ru/netology/Servts/CashbackTest.java
+++ src/test/java/ru/netology/Servts/CashbackTest.java
@@ -9,41 +9,41 @@ import static org.junit.Assert.*;
     public void serviceBonus() {
         Cashback service = new Cashback();
         int amount = 1000;
-        int expected = service.remain(amount);
-        int actual= 0;
-        assertEquals(actual,expected);
+        int actual = service.remain(amount);
+        int expected= 0;
+        assertEquals(expected,actual);
 
     }
     @Test
     public void upToBuyAt564() {
          Cashback service = new Cashback();
          int amount = 436;
-         int expected = service.remain(amount);
-         int actual = 564;
-         assertEquals(actual,expected);
+         int actual = service.remain(amount);
+         int expected = 564;
+         assertEquals(expected,actual);
      }
      @Test
      public void upToBuyAt200() {
          Cashback service = new Cashback();
          int amount = 800;
-         int expected = service.remain(amount);
-         int actual = 200;
-         assertEquals(actual,expected);
+         int actual = service.remain(amount);
+         int expected = 200;
+         assertEquals(expected,actual);
      }
      @Test
      public void upToBuyAt900() {
          Cashback service = new Cashback();
          int amount = 100;
-         int expected = service.remain(amount);
-         int actual = 900;
-         assertEquals(actual,expected);
+         int actual = service.remain(amount);
+         int expected = 900;
+         assertEquals(expected,actual);
      }
      @Test
      public void toBuy1200() {
          Cashback service = new Cashback();
          int amount = 1200;
-         int expected = service.remain(amount);
-         int actual = 800;
-         assertEquals(actual,expected);
+         int actual = service.remain(amount);
+         int expected = 800;
+         assertEquals(expected,actual);
      }
 }
