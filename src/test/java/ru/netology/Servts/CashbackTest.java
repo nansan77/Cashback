765409a61ec40465e2b38f457a83dc8dd2861944
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-03-09 22:15:52 +0300

    bonus

--- src/test/java/ru/netology/Servts/CashbackTest.java
+++ src/test/java/ru/netology/Servts/CashbackTest.java
@@ -4,46 +4,50 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
- public class CashbackTest {
+public class CashbackTest {
     @Test
     public void serviceBonus() {
         Cashback service = new Cashback();
         int amount = 1000;
         int actual = service.remain(amount);
-        int expected= 0;
-        assertEquals(expected,actual);
+        int expected = 0;
+        assertEquals(expected, actual);
 
     }
+
     @Test
     public void upToBuyAt564() {
-         Cashback service = new Cashback();
-         int amount = 436;
-         int actual = service.remain(amount);
-         int expected = 564;
-         assertEquals(expected,actual);
-     }
-     @Test
-     public void upToBuyAt200() {
-         Cashback service = new Cashback();
-         int amount = 800;
-         int actual = service.remain(amount);
-         int expected = 200;
-         assertEquals(expected,actual);
-     }
-     @Test
-     public void upToBuyAt900() {
-         Cashback service = new Cashback();
-         int amount = 100;
-         int actual = service.remain(amount);
-         int expected = 900;
-         assertEquals(expected,actual);
-     }
-     @Test
-     public void toBuy1200() {
-         Cashback service = new Cashback();
-         int amount = 1200;
-         int actual = service.remain(amount);
-         int expected = 800;
-         assertEquals(expected,actual);
-     }
+        Cashback service = new Cashback();
+        int amount = 436;
+        int actual = service.remain(amount);
+        int expected = 564;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void upToBuyAt200() {
+        Cashback service = new Cashback();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void upToBuyAt900() {
+        Cashback service = new Cashback();
+        int amount = 100;
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void toBuy1200() {
+        Cashback service = new Cashback();
+        int amount = 1200;
+        int actual = service.remain(amount);
+        int expected = 800;
+        assertEquals(expected, actual);
+    }
 }
