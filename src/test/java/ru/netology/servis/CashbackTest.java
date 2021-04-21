eb4b2533a8c0074f037f4af8c3cc10bd4abc51fe
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-04-21 20:00:28 +0300

    ADD

---
+++ src/test/java/ru/netology/servis/CashbackTest.java
@@ -0,0 +1,42 @@
+package ru.netology.servis;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackTest {
+
+    @Test
+    public void serviceBonus2() {
+        Cashback service = new Cashback();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void serviceBonus1() {
+        Cashback service = new Cashback();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void serviceBonus() {
+        Cashback service = new Cashback();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(expected, actual);
+    }
+}
